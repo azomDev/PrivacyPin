b9ee452d30d07c754cbafbc1425249682a8ac799
Kit <178309739+KitCatLove@users.noreply.github.com>
2025-05-30 15:29:49 -0400

    Made some changes to the UI on the home and create account page.

--- privacypin/src/routes/+page.svelte
+++ privacypin/src/routes/+page.svelte
@@ -1,13 +1,18 @@
 <script lang="ts">
 	import { Store } from "../utils/store";
 	import { goto } from '$app/navigation';
+	import "../app.css";
+	//import { Switch } from "m3-svelte";
 
 	(async () => {
 		const is_logged_in = await Store.isLoggedIn();
 		if (!is_logged_in) {
-			goto("/create-account");
+			await goto("/create-account");
 		} else {
-			goto("/home");
+			await goto("/home");
 		}
 	})();
+
+	//let darkTheme = $state(true);
 </script>
+<!-- <Switch bind:checked={darkTheme}/> -->
--- privacypin/vite.config.js
+++ privacypin/vite.config.js
@@ -1,12 +1,11 @@
 import { defineConfig } from "vite";
 import { sveltekit } from "@sveltejs/kit/vite";
 
-// @ts-expect-error process is a nodejs global
 const host = process.env.TAURI_DEV_HOST;
 
 // https://vitejs.dev/config/
 export default defineConfig(async () => ({
-	plugins: [sveltekit()],
+	plugins: [await sveltekit()],
 	// Vite options tailored for Tauri development and only applied in `tauri dev` or `tauri build`
 	//
 

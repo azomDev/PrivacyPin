9648e99848b500a93e11732ad615a5462d8e4c61
azomDev <dev@azom.ca>
2025-06-03 21:20:00 -0400

    made qol improvements

--- app/src-tauri/src/lib.rs
+++ app/src-tauri/src/lib.rs
@@ -1,7 +1,7 @@
 // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
 #[tauri::command]
 fn greet(name: &str) -> String {
-    format!("Hello, {}! You've been greeted from Rust!", name)
+    return format!("Hello, {}! You've been greeted from Rust!", name);
 }
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
